394f2241537be1b6fa581604b2aa0680e0cf2b6e
Brendon Fatland <b.fatland@gmail.com>
2019-10-11 12:50:51 -0500

    Conditional Rednering basics added.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: 40 };
+    this.state = { late: 40, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ late: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
-    return <div> Latitude: {this.state.late}</div>;
+    if (this.state.errorMessage && !this.state.late) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.late) {
+      return <div> error: {this.state.errorMessage}</div>;
+    }
+
+    return <div>Loading</div>;
   }
 }
 
