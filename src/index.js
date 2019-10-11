515c601a5c504dd9c0d7f99b5d68fdc1ae88ee55
Brendon Fatland <b.fatland@gmail.com>
2019-10-11 16:59:04 -0500

    Passed props to SeasonsDisplay. Added some ternary operators to sort the season configuration.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonsDisplay from "./SeasonsDisplay";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -17,7 +18,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div> Lattitude: {this.state.lat}</div>;
+      return <SeasonsDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading</div>;
